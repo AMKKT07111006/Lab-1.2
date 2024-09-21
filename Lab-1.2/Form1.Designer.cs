e8a1695a111199270ef6c2217a76f91b87d54ce3
SV <SV@PM18-PC30>
2024-09-21 09:05:02 +0700

    Add project files.

---
+++ Lab-1.2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab_1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(77, 42);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(77, 365);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(635, 365);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(635, 42);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(158, 42);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(471, 23);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(80, 77);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(131, 15);
+            this.label.TabIndex = 2;
+            this.label.Text = "Các process đang chạy:";
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.ItemHeight = 15;
+            this.lbOutput.Location = new System.Drawing.Point(82, 115);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(628, 244);
+            this.lbOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Quản lý process";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnBrowse;
+        private Button btnRefresh;
+        private Button btnStop;
+        private Button btnStart;
+        private TextBox txtPath;
+        private Label label;
+        private ListBox lbOutput;
+    }
+}
---
+++ Lab-1.2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace Lab_1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();   
+            //hiển thị danh sách các process ₫ang chạy khi vừa mở cửa sổ form lên
+            DisplayProcess();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //tạo form duyệt chọn file khả thi cần chạy
+            OpenFileDialog dlg = new OpenFileDialog();
+            //hiển thị form duyệt chọn file khả thi cần chạy
+            DialogResult ret = dlg.ShowDialog();
+            //kiểm tra quyết ₫ịnh của người dùng, nếu người dùng chọn OK thì ghi nhận tên file
+            if (ret == DialogResult.OK)
+                txtPath.Text = dlg.FileName;
+        }
+
+        void DisplayProcess()
+        {
+            //xóa nội dung cũ của ListBox
+            lbOutput.Items.Clear();
+            //tìm danh sách các process ₫ang chạy
+            Process[] plist = Process.GetProcesses();
+            //hiển thị tên, ID của từng process
+            foreach (Process process in plist)
+            {
+                lbOutput.Items.Add(process.ProcessName + ", " + process.Id);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //tạo mới ₫ối tượng quản lý Process.
+            Process myProcess = new Process();
+            try
+            {
+                //thiết lập ₫ường dẫn file cần chạy
+                myProcess.StartInfo.FileName = txtPath.Text;
+                //kích hoạt process
+                myProcess.Start();
+                //hiển thị danh sách các process ₫ang chạy
+                DisplayProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            //hiển thị danh sách các process ₫ang chạy
+            DisplayProcess();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //xác ₫ịnh tên, ID của process ₫ang ₫ược chọn ₫ể xóa
+            String[] param = lbOutput.SelectedItem.ToString().Split(',');
+            //tìm ₫ối tượng quản lý process có ID xác ₫ịnh
+            Process proc = Process.GetProcessById(Int32.Parse(param[1]));
+            //xóa process
+            proc.Kill();
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
